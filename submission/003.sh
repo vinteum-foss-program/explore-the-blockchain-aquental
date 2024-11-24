8315fa9acfbb3b06833e3dbc93abecbbee4743b7
aquental <antonio.quental@gmail.com>
2024-11-24 20:11:09 -0300

    fixed question three

--- submission/003.sh
+++ submission/003.sh
@@ -1,5 +1,5 @@
 # How many new outputs were created by block 123,456?
 hash=$(bitcoin-cli getblockhash 123456)
-block=$(bitcoin-cli getblock $hash)
-echo $block | jq -r '.tx[] | length' | wc -l
-# 13
+block=$(bitcoin-cli getblock $hash 2)
+echo $block | jq -r '.tx[].vout[].n ' | wc -l 
+# 24
